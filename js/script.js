c2fd74d4a9da48b8a4d227c08f549a156c1ae157
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-02-10 12:24:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const shareIconTwo = document.querySelector(".triangulo-para-baixo");
 button.addEventListener("click", submit);
 
 function submit() {
-  socialH.classList.remove("hidden");
-  shareIconTwo.classList.remove("hidden");
+  if (socialH.classList.contains("hidden")) {
+    socialH.classList.remove("hidden");
+    shareIconTwo.classList.remove("hidden");
+  } else {
+    socialH.classList.add("hidden");
+    shareIconTwo.classList.add("hidden");
+  }
 }
