64f7b7295db6d47b21d5d8c1193a0124f6dcbc7a
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-02-10 10:23:37 -0300

    style

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(".share-icon");
+const socialH = document.querySelector(".social");
+const shareIconTwo = document.querySelector(".triangulo-para-baixo");
+const return = 
+button.addEventListener("click", submit);
+
+function submit() {
+  socialH.classList.remove("hidden");
+  shareIconTwo.classList.remove("hidden");
+}
